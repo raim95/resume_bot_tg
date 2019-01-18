1ddb77c2c8df605fbe6eadec3f87c9d7b447ff22
raim95 <raim9595@gmail.com>
2019-01-18 12:37:16 +0300

    первая рабочая версия, без обучения

---
+++ bot.py
@@ -0,0 +1,27 @@
+# Настройки
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+updater = Updater(token='692923906:AAGQyhbVjjXDhi4HL2q1KxwQ2EERsuC0qAk') # Токен API к Telegram
+dispatcher = updater.dispatcher
+
+# Обработка команд
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, давай пообщаемся?')
+
+def textMessage(bot, update):
+    response = 'Получил Ваше сообщение: ' + update.message.text
+    bot.send_message(chat_id=update.message.chat_id, text=response)
+
+# Хендлеры
+start_command_handler = CommandHandler('start', startCommand)
+text_message_handler = MessageHandler(Filters.text, textMessage)
+
+# Добавляем хендлеры в диспетчер
+dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(text_message_handler)
+
+# Начинаем поиск обновлений
+updater.start_polling(clean=True)
+
+# Останавливаем бота, если были нажаты Ctrl + C
+updater.idle()

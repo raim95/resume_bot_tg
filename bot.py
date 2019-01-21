07d7338c7982f64e962bed4b2868e2cae205c646
raim95 <raim9595@gmail.com>
2019-01-21 20:20:53 +0300

    Должны отправляться файлы)) вместе с их айди))

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+# -*- config: utf-8 -*-
 import config
 import telebot
 
@@ -6,6 +7,15 @@ bot = telebot.TeleBot(config.token)
 @bot.message_handler(content_types=["text"])
 def repeat(message):
 	bot.send_message(message.chat.id, message.text)
+
+@bot.message_handler(commands=['pic'])
+def find_file_ids(message):
+	for file in os.listdir('pic/'):
+		if file.split('.')[-1]=='ogg':
+			f = open ('pic/'+file, 'rb')
+			msg = bot.send_photo(message.chat.id, f, None)
+			bot.send_message(message,chat.id, msg.photo.file_id, reply_to_message_id=msg.message_id)
+		time.sleep(3)
 
 if __name__ =='__main__':
 	bot.polling(none_stop = True)

9fc1dca91efc7dd628471be3d68df2b9ef17e10c
raim95 <raim9595@gmail.com>
2019-01-21 19:16:09 +0300

    не переключался в мастер

---
+++ config.py
@@ -0,0 +1,35 @@
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import apiai, json
+
+updater = Updater(token='642035956:AAG5VuVk81SI_McYQRhXvjAZipdJTeaUVHQ')
+#updater = Updater(token='692923906:AAGQyhbVjjXDhi4HL2q1KxwQ2EERsuC0qAk')
+dispatcher = updater.dispatcher
+
+def startCommand(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text='Привет, меня зовут как-нибудь ещё.')
+
+def textMessage(bot, update):
+	request = apiai.ApiAI('d19ec966a8314d22bacbe59da0dfc2a4').text_request()
+	request.lang = 'ru'
+	request.session_id = 'BatlabAIBot'
+	request.query = update.message.text
+	responseJson = json.loads(request.getresponse().read().decode('utf-8'))
+	response = responseJson['result']['fulfillment']['speech']
+	if response:
+		bot.send_message(chat_id=update.message.chat_id, text=response)
+	else:
+		bot.send_message(chat_id=update.message.chat_id, text='Do not undestand')
+
+start_command_handler = CommandHandler('start', startCommand)
+text_message_handler = MessageHandler(Filters.text, textMessage)
+
+
+dispatcher.add_handler(start_command_handler)
+dispatcher.add_handler(text_message_handler)
+
+
+updater.start_polling(clean=True)
+
+
+updater.idle()

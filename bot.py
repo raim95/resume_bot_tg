bcae913c500b75bbaa0c671bfae16d996c1890c8
raim95 <raim9595@gmail.com>
2019-01-22 19:19:20 +0300

    Пробуем дописать логи, надо выгрузить айдишники файлов

--- bot.py
+++ bot.py
@@ -13,10 +13,15 @@ bot = telebot.TeleBot(config.token)
 @bot.message_handler(commands=['pic'])
 def find_file_ids(message):
 	for file in os.listdir('pic/'):
+		file = open('files_id.txt', 'w')
+		file.close()
 		if file.split('.')[-1]=='jpg':
 			f = open ('pic/'+file, 'rb')
 			msg = bot.send_photo(message.chat.id, f)
-			bot.send_message(message.chat.id, msg.photo[0].file_id, reply_to_message_id=msg.message_id)
+			file_id = bot.send_message(message.chat.id, msg.photo[0].file_id, reply_to_message_id=msg.message_id)
+			f = open('files_id.txt', 'a')
+			f.write(file_id +'\n')
+			f.close()
 		time.sleep(3)
 
 if __name__ =='__main__':

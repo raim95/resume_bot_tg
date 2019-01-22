bd1827fbfba9225def0031c16c7a2ab543b39cc5
raim95 <raim9595@gmail.com>
2019-01-22 17:22:52 +0300

    Пробуем не фото, а документ

--- bot.py
+++ bot.py
@@ -15,8 +15,9 @@ def find_file_ids(message):
 	for file in os.listdir('pic/'):
 		if file.split('.')[-1]=='jpg':
 			f = open ('pic/'+file, 'rb')
-			msg = bot.send_photo(message.chat.id, f, None)
-			bot.send_message(message.chat.id, msg.file_id, reply_to_message_id=msg.message_id)
+			msg = bot.send_document(message.chat.id, f, None)
+			bot.send_message(message.chat.id, type(msg))
+			bot.send_message(message.chat.id, msg.document.file_id, reply_to_message_id=msg.message_id)
 		time.sleep(3)
 
 if __name__ =='__main__':

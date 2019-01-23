6398c567686d14ad94c6c32576f0736320caf541
raim95 <raim9595@gmail.com>
2019-01-24 00:03:17 +0300

    Приветственая строчка перенесена в конфиг

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 token = '642035956:AAG5VuVk81SI_McYQRhXvjAZipdJTeaUVHQ' #amazon_version
 #token = '692923906:AAGQyhbVjjXDhi4HL2q1KxwQ2EERsuC0qAk' #master
+himsg = 'Здравствуйте! Я бот-резюме. Узнать некоторые факты обо мне ты сможешь с помощью меню внизу. Если ты потерял меню, оно откроется по нажатию иконки из квадратиков возле значка камеры."'

021b7250ee7e85024fb609081a9fd176f2005f0e
raim95 <raim9595@gmail.com>
2019-01-23 20:09:44 +0300

    +gitignor, добавление кнопок

--- bot.py
+++ bot.py
@@ -3,9 +3,17 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
+@bot.message_handler(commands=["start"])
+def keys(message):
+	key = types.ReplyKeyboardMarkup()
+	key.row("1","2","3")
+	bot.send_message(message.chat.id, "Выберите цифру:", reply_markup=key)
+
 @bot.message_handler(content_types=["text"])
 def repeat(message):
 	bot.send_message(message.chat.id, message.text)
+
+
 
 if __name__ =='__main__':
 	bot.polling(none_stop = True)

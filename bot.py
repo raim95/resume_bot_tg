1db3eaf50fd5d631d690e082c9db593926af769f
raim95 <raim9595@gmail.com>
2019-02-01 19:49:52 +0300

    Угадай картинку, первая версия, перед тестами. Работает на Альтроне

--- bot.py
+++ bot.py
@@ -1,44 +1,62 @@
+# -*- config: utf-8 -*-
+import config
+import telebot
+import os
+import time
 
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import apiai, json, telegram
+bot = telebot.TeleBot(config.token)
 
-updater = Updater(token='642035956:AAG5VuVk81SI_McYQRhXvjAZipdJTeaUVHQ')
-#updater = Updater(token='692923906:AAGQyhbVjjXDhi4HL2q1KxwQ2EERsuC0qAk')
-dispatcher = updater.dispatcher
+@bot.message_handler(commands=['pic'])
+def find_file_ids(message):
+	file = open('files_id.txt', 'w')
+	file.close()
+	for file in os.listdir('pic/'):
+		if file.split('.')[-1]=='jpg':
+			f = open ('pic/'+file, 'rb')
+			msg = bot.send_photo(message.chat.id, f)
+			bot.send_message(message.chat.id, msg.photo[0].file_id, reply_to_message_id=msg.message_id)
+			file_id = msg.photo[0].file_id
+			f = open('files_id.txt', 'a')
+			f.write(file_id +'\n')
+			f.close()
+		time.sleep(3)
+		
+@bot.message_handler(commands=['game'])
+def game(message):
+    # Подключаемся к БД
+    db_worker = SQLighter(config.database_name)
+    # Получаем случайную строку из БД
+    row = db_worker.select_single(random.randint(1, utils.get_rows_count()))
+    # Формируем разметку
+    markup = utils.generate_markup(row[2], row[3])
+    # Отправляем аудиофайл с вариантами ответа
+    bot.send_voice(message.chat.id, row[1], reply_markup=markup)
+    # Включаем "игровой режим"
+    utils.set_user_game(message.chat.id, row[2])
+    # Отсоединяемся от БД
+    db_worker.close()
+	
 
-def keys(bot, update):
-	key_1 = telegram.keyboardButton('1')
-	key_2 = telegram.keyboardButton('1')
-	key_3 = telegram.keyboardButton('1')
-	key_4 = telegram.keyboardButton('1')
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def check_answer(message):
+    # Если функция возвращает None -> Человек не в игре
+    answer = utils.get_answer_for_user(message.chat.id)
+    # Как Вы помните, answer может быть либо текст, либо None
+    # Если None:
+    if not answer:
+        bot.send_message(message.chat.id, 'Чтобы начать игру, выберите команду /game')
+    else:
+        # Уберем клавиатуру с вариантами ответа.
+        keyboard_hider = types.ReplyKeyboardRemove()
+        # Если ответ правильный/неправильный
+        if message.text == answer:
+            bot.send_message(message.chat.id, 'Верно!', reply_markup=keyboard_hider)
+        else:
+            bot.send_message(message.chat.id, 'Увы, Вы не угадали. Попробуйте ещё раз!', reply_markup=keyboard_hider)
+        # Удаляем юзера из хранилища (игра закончена)
+        utils.finish_user_game(message.chat.id)
 
-def keyboard(bot, update):
-	board = telegram.ReplyKeyboardMarkup(keys)
-
-def startCommand(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, ReplyKeyboardMarkup = board, text='Привет, давай пообщаемся?')
-
-def textMessage(bot, update):
-	request = apiai.ApiAI('d19ec966a8314d22bacbe59da0dfc2a4').text_request()
-	request.lang = 'ru'
-	request.session_id = 'BatlabAIBot'
-	request.query = update.message.text
-	responseJson = json.loads(request.getresponse().read().decode('utf-8'))
-	response = responseJson['result']['fulfillment']['speech']
-	if response:
-		bot.send_message(chat_id=update.message.chat_id, text=response)
-	else:
-		bot.send_message(chat_id=update.message.chat_id, text='Do not undestand')
-
-start_command_handler = CommandHandler('start', startCommand)
-text_message_handler = MessageHandler(Filters.text, textMessage)
-
-
-dispatcher.add_handler(start_command_handler)
-dispatcher.add_handler(text_message_handler)
-
-
-updater.start_polling(clean=True)
-
-
-updater.idle()
+if __name__ == '__main__':
+    utils.count_rows()
+    random.seed()
+    bot.polling(none_stop=True)

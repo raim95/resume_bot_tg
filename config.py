d16ceb77c4cdba54c58df18c4373fcec925eded7
raim95 <raim9595@gmail.com>
2019-02-04 17:58:20 +0300

    Добавил названия БД

--- config.py
+++ config.py
@@ -1,2 +1,5 @@
 token = '642035956:AAG5VuVk81SI_McYQRhXvjAZipdJTeaUVHQ' #ultron
 #token = '692923906:AAGQyhbVjjXDhi4HL2q1KxwQ2EERsuC0qAk' #bot.lexi
+
+shelve_name = 'tg_bot'
+database_name = 'tg_bot.db'
